5efe85342149bca0ceb8de49b5daeab28942a0e0
VERNON HEZRON <103187250+HeTheGYPSY@users.noreply.github.com>
2022-09-08 18:07:34 +0300

    Final Edits

--- Others.py
+++ Others.py
@@ -1,114 +1,50 @@
-import random
-from flask import current_app, jsonify, app
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+
+# The ID of a sample document.
+DOCUMENT_ID = str(input("Enter a document ID: "))
 
 
-class PHlo:
-
-    def generate_code(self):
-        code = random.choice(range(100000, 999999))  # generating 6 digit random code
-        return code
-
-    def send_verification_code_phlo(self, dst_number, code, mode):
-            payload = {
-                "from": self.app_number,
-                "to": dst_number,
-                "otp": code,
-                "mode": mode,
-            }
-            try:
-                phlo = self.client_phlo.phlo.get(self.phlo_id)
-                response = phlo.run(**payload)
-                return response
-            except Exception as e:
-                print(e)
-                return "Error encountered", 400
-
-    @app.route("/checkcode/<number>/<code>")
-    def check_code(number, code):
-        """
-        check_code(number, code) accepts a number and the code entered by the user and
-        tells if the code entered for that number is correct or not
-        """
-        original_code = current_app.redis.get("number:%s:code" % number)
-        if original_code == code:  # verification successful, delete the code
-            current_app.redis.delete("number:%s:code" % number)
-            return jsonify({"status": "success", "message": "codes match, number verified"}), 200,
-        elif original_code != code:
-            return (
-                jsonify(
-                    {
-                        "status": "rejected",
-                        "message": "codes do not match, number not verified",
-                    }
-                ),
-                404,
-            )
+def main():
+    """Shows basic usage of the Docs API.
+    Prints the title of a sample document.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
         else:
-            return jsonify({"status": "failed", "message": "number not found"}), 500
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('docs', 'v1', credentials=creds)
+
+        # Retrieve the documents contents from the Docs service.
+        document = service.documents().get(documentId=DOCUMENT_ID).execute()
+
+        print('The title of the document is: {}'.format(document.get('title')))
+    except HttpError as err:
+        print(err)
 
 
-class API:
-
-    def generate_code(self):
-        code = random.choice(range(100000, 999999))  # generating 6-digit random code
-        return code
-
-    def send_verification_code_sms(self, dst_number: str, message):
-        """
-        `send_verification_code` accepts destination number
-        to which the message that has to be sent.
-
-        The message text should contain a `__code__` construct
-        in the message text which will be
-        replaced by the code generated before sending the SMS.
-
-        :param: dst_number
-        :param: message
-        :return: verification code
-        """
-        try:
-            response = self.client.messages.create(
-                src=self.app_number, dst=dst_number, text=message
-            )
-            print(response)
-            return response
-        except Exception as e:
-            print(e)
-            return "Error encountered", 400
-
-    def send_verification_code_voice(self, dst_number, code):
-        try:
-            response = self.client.calls.create(
-                from_=self.app_number,
-                to_=dst_number,
-                answer_url=f"https://twofa-answerurl.herokuapp.com/answer_url/{code}",
-                answer_method="GET",
-            )
-            return response
-        except Exception as e:
-            print(e)
-            return "Error encountered", 400
-
-    @app.route("/checkcode/<number>/<code>")
-    def check_code(self, code):
-        """
-        check_code(number, code) accepts a number and the code entered by the user and
-        tells if the code entered for that number is correct or not.
-        """
-
-        original_code = current_app.redis.get("number:%s:code" % self)
-        if original_code == code:  # verification successful, delete the code
-            current_app.redis.delete("number:%s:code" % self)
-            return jsonify({"status": "success", "message": "codes match, number verified"}), 200,
-        elif original_code != code:
-            return (
-                jsonify(
-                    {
-                        "status": "rejected",
-                        "message": "codes do not match, number not verified",
-                    }
-                ),
-                404,
-            )
-        else:
-            return jsonify({"status": "failed", "message": "number not found"}), 500
+if __name__ == '__main__':
+    main()
