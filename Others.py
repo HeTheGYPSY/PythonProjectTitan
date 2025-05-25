fdc00d4345af336cee492d7dbeced0dea06905e9
VERNON HEZRON <103187250+HeTheGYPSY@users.noreply.github.com>
2025-05-25 13:08:59 +0300

    Update Others.py

--- Others.py
+++ Others.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 
 
@@ -9,5 +10,28 @@ def installations():
         print("Installation Completed!")
 
 
+def listing():
+    listing = subprocess.run("pip list", capture_output=True).stdout.decode()
+    for i in range(0, 10):
+        for j in range(0, 10):
+            for k in range(0, 10):
+                arguments = set(re.findall(f"(.*)=={i}.{j}.{k}", listing))
+    edited = []
+    for item in arguments:
+        if item != '':
+            edited.append(item)
+    print(edited)
+
+
+def lister():
+    listing = subprocess.run("pip list", capture_output=True).stdout.decode()
+    arguments = set(re.findall(f"(.*)\s\s", listing))
+    edited = []
+    for item in arguments:
+        if item != '':
+            edited.append(item.rstrip("\s(.*)"))
+    print(edited)
+
+
 if __name__=='__main__':
-    installations()
+    lister()
